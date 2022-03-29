bb88fa10daa1262244fba2b4bd2d626a2f65cf66
Kenny William Nyallau <you@example.com.env>
2022-03-29 16:25:19 +0800

    Update components arrangements

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import NavbarComp from './NavbarComp.js';
 import Jumbo from './JumboComp.js';
+import MyJumbo from './components/MyJumbo.js';
 import AboutPage from './About.js';
+import AboutMe from './components/About.js';
 import CornPage from './Corn.js';
 import ProjectPage from './Project.js';
 import SkillsPage from './skills.js';
-import TimelinePage from './Timeline.js';
+import Timeline from './components/Timeline.js';
+//import TimelinePage from './Timeline.js';
 import ContactPage from './Contact.js';
-import FooterPage from './Footer.js';
+//import FooterPage from './Footer.js';
+import FooterPage from './components/Footer.js';
 import './App.css';
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <Fragment>
-      <NavbarComp />
-      <AboutPage />
-      <Jumbo />
-      <ProjectPage />
-     
-      <SkillsPage />
-      <TimelinePage/>
-      <ContactPage/>
-      <FooterPage/>
-      </Fragment>
+      <>
+        <NavbarComp />
+        <MyJumbo/>
+        
+        {/*<Jumbo />*/}
+        <ProjectPage />
+        <SkillsPage />
+        {/* <TimelinePage /> */}
+        
+        <Timeline/>
+        <AboutPage />
+        <ContactPage />
+        
+        <FooterPage />
+      </>
 
     );
   }
