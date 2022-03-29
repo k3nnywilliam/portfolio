4ca5ac723602d35d3911ddd44da5ce36a8133157
Kenny William Nyallau <you@example.com.env>
2022-03-29 16:21:56 +0800

    Update about me page

--- src/About.js
+++ src/About.js
@@ -1,34 +1,27 @@
-import React, { Fragment, } from 'react';
+import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
-import me from './images/me8.jpg';
+import me from './images/me5.jpg';
 import styles from './App.css';
 
 const AboutPage = () => {
-    return [
-        <Fragment>
+    return (
+        <>
             <Container>
                 <br /><br />
                 <div id='about'></div>
                 <h1>About Me</h1>
-                <br/>
+                <br />
                 <Row>
-                <Col md={4}>
-                <Image src={me} rounded className={styles.me_pic} />
-                </Col>
+                    <Col md={4} >
+                        <Image src={me} rounded={true} className={styles.me_pic} />
+                    </Col>
                     <Col>
-                    <br/>
+                        <br />
                         <p>
-                            My name is Kenny William Nyallau and I am a full stack software developer from Kuching, Malaysia.I was born in the Borneo Island, where I belong to a native tribe called Iban.I graduated from the University of South Australia in 2011 with Masters of Information Technology to
-                            pursue a career in software engineering and computer science.
-                        </p>
-                        <p> In the past, I have worked as research associate at one of the research labs in University South Australia.The research focus at
-                            that time was in visuo-haptic augmented reality (AR), data visualization and AR projection mapping.
-                            From 2018 to end of 2019, I was the computer vision lead developer with a startup company in Munich, Germany.</p>
-                        <p>My past work experiences include developing microservices-based computer vision system for identifying multi-channel live video broadcasts, public interactive visualization, as well as AR mobile apps and head mounted display.</p>
-                        <p>My 8 years of professional experience has allowed me to work with many talented collaborators from different parts of the world including United States, Australia, Germany, Japan, South Korea and Singapore.</p>
 
-                        <p>Recently I have decided to switch away from my career in augmented reality to focus more on impactful contribution towards human society as well as the environment.So now I am learning how use geospatial data to identify interesting trends and patterns in domains such as food production, healthcare and crop and livestock monitoring.
-                            I believe that such analysis in those domains would prepare us for the impending food and health crisis due to climate emergency (climate change is already happening on the extreme level, so it doesn't make sense to call it climate change anymore).
+                            💼 Software developer | Interested in life science, nutrition, healthcare, food security and geospatial visualization
+                            ⚡ Fun fact: I belong to a native tribe called Iban in Malaysia and I used to be a 3D artist 🧑‍🎨
+
                         </p>
                         <p>When I'm not in front of my computer, I like to read, draw, practice yoga and learn calisthenics.</p>
 
@@ -36,9 +29,9 @@ const AboutPage = () => {
                     </Col>
                 </Row>
             </Container>
-        </Fragment >
+        </ >
 
-    ];
+    );
 }
 
 export default AboutPage;
