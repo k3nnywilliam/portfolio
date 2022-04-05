5c8e788be16a455b089a749a13851c0058e3519d
Kenny William Nyallau <you@example.com.env>
2022-04-05 15:04:41 +0800

    Replace new jumbo gif

--- src/components/MyJumbo.js
+++ src/components/MyJumbo.js
@@ -1,20 +1,21 @@
 import React from "react";
 import img1 from "../images/data-analysis1.png"
+import dataviz from "../images/dataviz1.gif"
 import graph from "../images/graph.gif"
 
 function MyJumbo() {
     return (
         <>
             <div className="jumbotron jumbotron-fluid bg-black " style={{
-                    backgroundImage: `url(${graph}) `,
+                    backgroundImage: `url(${dataviz}) `,
                     backgroundRepeat: "no-repeat",
                     backgroundSize: "cover",
                 }}>
                 <div className="container vh-100 d-flex align-items-center justify-content-center" >
                     <div className="position-absolute ">
              
-                        <h1 className="text-center"> Kenny William Nyallau</h1>
-                        <p className="lead text-center">Full stack developer | Creative Technologist | Data Visualization</p>
+                        <h1 className="text-center text-light"> Kenny William Nyallau</h1>
+                        <p className="lead text-center text-light font-weight-bold">Full stack developer | Creative Technologist | Data Visualization</p>
                     </div>
 
                 </div>
