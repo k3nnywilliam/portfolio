f045b13dac99c1ad413067b6a5f81b7ada5c74ec
Kenny William Nyallau <you@example.com.env>
2022-08-09 19:55:29 +0800

    Updated skills

--- src/skills.js
+++ src/skills.js
@@ -11,19 +11,19 @@ const SkillsPage = () => {
                     <h1>Skills</h1>
                     <br/>
                     <h3>Programming</h3>
-                    <p>Python, Javascript, C#, SQL, Bash, C++</p>
+                    <p>Python, Javascript, R, C#, SQL, Bash</p>
                     <h3>Data Analysis/Visualization</h3>
                     <p>Matplotlib, Pandas, Scikit Learn, NumPy, Seaborn, Plotly, D3.js</p>
                     <h3>2D/3D Graphics Visualization</h3>
                     <p>DeckGL, ThreeJS</p>
-                    <h3>Geospatial Analysis/Visualization</h3>
+                    <h3>Geospatial Analysis</h3>
                     <p>Cesium, GeoPandas, Folium, Mapbox, LeafletJS</p>
                     <h3>Web development</h3>
-                    <p>HTML5, CSS, Sass, React, Node, Express, Vue, Flask</p>
+                    <p>HTML5, CSS, Sass, React, Node, Express, Vue, Flask, R Shiny</p>
                     <h3>Cloud platforms/tools</h3>
-                    <p>AWS, Docker, Terraform, Heroku, Digital Ocean, Netlify, Vercel</p>
+                    <p>Heroku, Digital Ocean, Netlify, Vercel</p>
                     <h3>CI/CD</h3>
-                    <p>CircleCI, Travis CI, GitHub Actions</p>
+                    <p> Jenkins, CircleCI, Travis CI, GitHub Actions</p>
                 </div>
             </Container>
         </Fragment>
